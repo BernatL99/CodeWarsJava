6da520c882c12f122e0317cccf4d898983683410
BernatL99 <116670667+BernatL99@users.noreply.github.com>
2024-02-07 20:51:47 +0100

    Create BattleshipFieldValidator.java

---
+++ Ejercicios/BattleshipFieldValidator.java
@@ -0,0 +1,24 @@
+//Prueba de 
+public class BattleField {
+    
+    public static boolean fieldValidator(int[][] field) {
+      int acorazado = 0, crucero = 0, destructor = 0, submarino = 0;
+      
+      for(int i = 0; i < field.length; i++){
+        for(int j = 0 ; j < field[i].length; j++){
+          
+          //Encontrar acorazado de manera orizontal
+          if((field[i][j] == 1) && (j - field.length >= 4)){
+            int count = 0;
+            count = field[i][j] + field[i][j+1] + field[i][j+2]+ field[i][j+3];
+            if(count == 4)acorazado++;
+          }else if((field[i][j] == 1) && (i - field.length >= 4)){
+            int count = 0;
+            count = field[i][j] + field[i+1][j] + field[i+2][j]+ field[i+3][j];
+            if(count == 4)acorazado++;
+        }
+      }
+    }
+  }  
+  return false;
+}
